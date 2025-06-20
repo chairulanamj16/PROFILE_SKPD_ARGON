03cc5e1ce8a12359d6cd24935da628e07a0a2ce3
izzoel <ramadhani.zulfahmi@gmail.com>
2025-06-20 10:09:58 +0800

    + Menu Video, Publikasi & PPID

---
+++ app/Http/Controllers/Backend/V1/PpidController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\backend\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class PpidController extends Controller
+{
+    protected $versi;
+
+    public function __construct()
+    {
+        $this->versi = 'v1';
+    }
+
+    public function index(Request $request, $account)
+    {
+        $data['account'] = $account;
+        return view('backend.' . $this->versi . '.' . request()->segment(1) . '.index', $data);
+    }
+}
---
+++ app/Livewire/V1/Video/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\V1\Video;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.v1.video.index');
+    }
+}
