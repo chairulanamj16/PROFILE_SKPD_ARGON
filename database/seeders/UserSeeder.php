05baa958f0c037d5694ad469b00165ee3f09881b
chairulanamj16 <chairuluniska@gmail.com>
2025-06-21 15:45:30 +0800

    V1

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User; // Sesuaikan dengan namespace model User Anda
+use App\Models\V1\Office;
 use Illuminate\Support\Facades\Hash;
 use Spatie\Permission\Models\Role;
 
@@ -17,16 +18,23 @@ class UserSeeder extends Seeder
     public function run()
     {
         // User admin
-        $admin = User::create([
-            'name' => 'Admin',
-            'username' => 'admin',
+        $user = User::create([
+            'name' => 'Dinas Komunikasi dan Informatika',
+            'username' => 'diskominfo',
             'email' => 'admin@mail.com',
             'password' => Hash::make('password'), // Ganti dengan password yang diinginkan
             'rule' => 'Super Admin'
         ]);
 
+        $office = Office::create([
+            'user_id' => $user->id,
+            'name' => 'Dinas Komunikasi dan Informatika',
+            'subdomain' => 'diskominfo',
+            'theme_gallery_id' => 1,
+        ]);
+
         // Assign role admin to user
         $adminRole = Role::findByName('Admin');
-        $admin->assignRole($adminRole);
+        $user->assignRole($adminRole);
     }
 }
